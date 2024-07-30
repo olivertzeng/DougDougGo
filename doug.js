e56a5fffb79461ff5a14f97f0aa4d6d1e5c1f1c5
Oliver Tzeng <olivertzeng@proton.me>
2024-07-30 23:30:21 +0800

    fix(doug.js): js great again

--- doug.js
+++ doug.js
@@ -9,44 +9,42 @@
 // @description DougDougGo is now your new search engine
 // @description:zh-TW  DougDougGo 將是你獨一無二的搜尋引擎
 
+const searchFilters = document.querySelector(".js-search-filters");
 const ike = document.createElement("a");
 const ikeAudio = new Audio(browser.runtime.getURL("assets/ike.mp3"));
-
-document.addEventListener("DOMContentLoaded", () => {
-	const searchFilters = document.querySelector(".js-search-filters");
-	searchFilters.appendChild(ike);
-});
 
 function bellPeper() {
 	const fav = document.createElement("link");
 	fav.rel = "icon";
-	fav.href = browser.runtime.getURL("assets/pepper.png");
+	fav.href = chrome.runtime.getURL("assets/pepper.png");
 	document.head.appendChild(fav);
 }
 
 function searchIke() {
 	const fav = document.createElement("link");
 	fav.rel = "icon";
-	fav.href = browser.runtime.getURL("assets/ike.gif");
+	fav.href = chrome.runtime.getURL("assets/ike.gif");
 	document.head.appendChild(fav);
 }
 
 function modifyText() {
 	const searchInput = document.querySelector("#search_form_input");
-	if (searchInput?.value?.toLowerCase().includes("ike")) {
-		ike.textContent = "You just searched Ike, dumbass";
-		ike.href = "https://www.youtube.com/watch?v=JsYWZSTbEPU";
-		document.title = "You just searched Ike, dumbass";
-		searchIke();
-		ikeAudio.play();
-	} else {
-		bellPeper();
+	// detect if the searchbar includes "ike"
+	// done by converting searchbar value t
+	if (searchInput?.value) {
+		const searchTerm = searchInput.value.toLowerCase().replace(/\s+/g, "");
+		if (searchTerm.includes("ike")) {
+			ike.textContent = "You just searched Ike, dumbass";
+			ike.href = "https://www.youtube.com/watch?v=JsYWZSTbEPU";
+			searchFilters.appendChild(ike);
+			document.title = "You just searched Ike, dumbass";
+			searchIke();
+			ikeAudio.play();
+		} else {
+			bellPeper();
+		}
 	}
 }
 
-// Add event listener to search input field
-document
-	.querySelector("#search_form_input")
-	.addEventListener("input", modifyText);
-
 modifyText();
+bellPeper();
