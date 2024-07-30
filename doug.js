b73506a5cc17e5602451f5463725fb445ee0197a
Oliver Tzeng <olivertzeng@proton.me>
2024-07-30 22:13:51 +0800

    bruh

--- doug.js
+++ doug.js
@@ -1,34 +1,33 @@
-// ==UserScript==
 // @name        DougDougGo JS
 // @name:zh-TW  DougDougGo JS
 // @namespace   https://duckduckgo.com
 // @match       *://*duckduckgo.com/*
 // @license     GPLv3
 // @grant       none
-// @version     1.1
+// @version     1.0
 // @author      Oliver Tzeng
 // @description DougDougGo is now your new search engine
 // @description:zh-TW  DougDougGo 將是你獨一無二的搜尋引擎
-// ==/UserScript==
+
+const ike = document.createElement("a");
+const ikeAudio = new Audio(browser.runtime.getURL("assets/ike.mp3"));
 
 document.addEventListener("DOMContentLoaded", () => {
 	const searchFilters = document.querySelector(".js-search-filters");
-	const ike = document.createElement("a");
-	const ikeAudio = new Audio("assets/ike.mp3");
+	searchFilters.appendChild(ike);
 });
 
 function bellPeper() {
 	const fav = document.createElement("link");
 	fav.rel = "icon";
-	fav.href = "https://i.imgur.com/6O47rNT.png";
+	fav.href = browser.runtime.getURL("assets/pepper.png");
 	document.head.appendChild(fav);
 }
 
 function searchIke() {
 	const fav = document.createElement("link");
 	fav.rel = "icon";
-	fav.href =
-		"https://static.wixstatic.com/media/08e73c_bd7c7956c35f494ebade57f2e26ba49a~mv2.gif";
+	fav.href = browser.runtime.getURL("assets/ike.gif");
 	document.head.appendChild(fav);
 }
 
@@ -38,7 +37,6 @@ function modifyText() {
 		ike.textContent = "You just searched Ike, dumbass";
 		ike.href = "https://www.youtube.com/watch?v=JsYWZSTbEPU";
 		document.title = "You just searched Ike, dumbass";
-		searchFilters.appendChild(ike);
 		searchIke();
 		ikeAudio.play();
 	} else {
