075da2b86088b837901d3e72f03a7b9ee384d9ed
Oliver Tzeng <olivertzeng@proton.me>
2024-07-30 20:48:52 +0800

    feat(version): bump version

--- doug.js
+++ doug.js
@@ -11,30 +11,30 @@
 // @description:zh-TW  DougDougGo 將是你獨一無二的搜尋引擎
 // ==/UserScript==
 
-const searchFilters = document.querySelector(".js-search-filters");
-const ike = document.createElement("a");
-const ikeAudio = new Audio("assets/ike.mp3");
+document.addEventListener("DOMContentLoaded", () => {
+	const searchFilters = document.querySelector(".js-search-filters");
+	const ike = document.createElement("a");
+	const ikeAudio = new Audio("assets/ike.mp3");
+});
 
-// Changes favicon to a weird bell pepper
 function bellPeper() {
-	var fav = document.createElement("link");
+	const fav = document.createElement("link");
 	fav.rel = "icon";
 	fav.href = "https://i.imgur.com/6O47rNT.png";
 	document.head.appendChild(fav);
 }
 
 function searchIke() {
-	var fav = document.createElement("link");
+	const fav = document.createElement("link");
 	fav.rel = "icon";
 	fav.href =
 		"https://static.wixstatic.com/media/08e73c_bd7c7956c35f494ebade57f2e26ba49a~mv2.gif";
 	document.head.appendChild(fav);
 }
 
-// You just said Ike, dumbass
 function modifyText() {
-	var searchInput = document.querySelector("#search_form_input");
-	if (searchInput && searchInput.value.toLowerCase().includes("ike")) {
+	const searchInput = document.querySelector("#search_form_input");
+	if (searchInput?.value?.toLowerCase().includes("ike")) {
 		ike.textContent = "You just searched Ike, dumbass";
 		ike.href = "https://www.youtube.com/watch?v=JsYWZSTbEPU";
 		document.title = "You just searched Ike, dumbass";
@@ -46,6 +46,9 @@ function modifyText() {
 	}
 }
 
-bellPeper();
+// Add event listener to search input field
+document
+	.querySelector("#search_form_input")
+	.addEventListener("input", modifyText);
+
 modifyText();
-playIke();
