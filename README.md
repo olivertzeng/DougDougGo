2b8bb9c5b233a7c93074d07b4d53ce73d742065b
Oliver Tzeng <olivertzeng@proton.me>
2024-07-30 18:16:00 +0800

    feat(extension): deprecates other stuff

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 ![DougDougGo](https://i.imgur.com/0xi9Gtg.png)
 
 **DougDougGo is now your new search engine!**
-* You can install DougDougGo JS [here](https://greasyfork.org/en/scripts/502061-dougdouggo-js) with
-* Violentmonkey
-* And install DougDougGo Stylus [here](https://userstyles.world/style/17417/dougdouggo-stylus)
+Things below are deprecated and will be taken down soon. Wait for the firefox extension.
+~~* You can install DougDougGo JS [here](https://greasyfork.org/en/scripts/502061-dougdouggo-js)
+with Violentmonkey~~
+~~* And install DougDougGo Stylus [here](https://userstyles.world/style/17417/dougdouggo-stylus)~~
 * Also check out DougDougify by [MagicJinn](https://github.com/MagicJinn/) [here](https://addons.mozilla.org/zh-TW/firefox/addon/youtube-dougdougify/)
 
 Issues and suggestions are welcomed
--- doug.js
+++ doug.js
@@ -13,47 +13,37 @@
 
 const searchFilters = document.querySelector(".js-search-filters");
 const ike = document.createElement("a");
-
-// kudos to https://greasyfork.org/en/scripts/4908-play-tone
-function playIke(e){
-  // create audio tag for WAV file
-  var el = document.createElement("audio");
-  el.setAttribute("autoplay", "autoplay");
-  // grab tone file from my site; to avoid mixed content, omit the protocol
-  el.setAttribute("src", "//download1511.mediafire.com/03ahvuacfungbBA7IIJnR1E089LtH73FVcKJxsXDYP99oJebk0y36nfXuQgQSy1E3E3H0yFfeA0UrhsplD0__0NA_ptssqjg8zMyrn1ZL4H8pclemfPjWK-ccMGi2uCK6ReEVz3iWkdleAmZX3RvBjd56BQYXumz5T6X8ZYCIHpISw/u6ty7mdp5zrk1as/ike.mp3");
-  // add to document body
-  document.body.appendChild(el);
-}
-
+const ikeAudio = new Audio("assets/ike.mp3");
 
 // Changes favicon to a weird bell pepper
 function bellPeper() {
-    var fav = document.createElement("link");
-    fav.rel = "icon";
-    fav.href = "https://i.imgur.com/6O47rNT.png";
-    document.head.appendChild(fav);
+	var fav = document.createElement("link");
+	fav.rel = "icon";
+	fav.href = "https://i.imgur.com/6O47rNT.png";
+	document.head.appendChild(fav);
 }
 
 function searchIke() {
-    var fav = document.createElement("link");
-    fav.rel = "icon";
-    fav.href = "https://static.wixstatic.com/media/08e73c_bd7c7956c35f494ebade57f2e26ba49a~mv2.gif";
-    document.head.appendChild(fav);
+	var fav = document.createElement("link");
+	fav.rel = "icon";
+	fav.href =
+		"https://static.wixstatic.com/media/08e73c_bd7c7956c35f494ebade57f2e26ba49a~mv2.gif";
+	document.head.appendChild(fav);
 }
 
 // You just said Ike, dumbass
 function modifyText() {
-    var searchInput = document.querySelector('#search_form_input');
-    if (searchInput && searchInput.value.toLowerCase().includes("ike")) {
-        ike.textContent = "You just searched Ike, dumbass";
-        ike.href = 'https://www.youtube.com/watch?v=JsYWZSTbEPU';
-        document.title = "You just searched Ike, dumbass";
-        searchFilters.appendChild(ike);
-        searchIke();
-        playIke();
-    } else{
-        bellPeper();
-    }
+	var searchInput = document.querySelector("#search_form_input");
+	if (searchInput && searchInput.value.toLowerCase().includes("ike")) {
+		ike.textContent = "You just searched Ike, dumbass";
+		ike.href = "https://www.youtube.com/watch?v=JsYWZSTbEPU";
+		document.title = "You just searched Ike, dumbass";
+		searchFilters.appendChild(ike);
+		searchIke();
+		ikeAudio.play();
+	} else {
+		bellPeper();
+	}
 }
 
 bellPeper();
